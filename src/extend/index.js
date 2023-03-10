1b15fa61f5c044b5d68912bb1e0a7508ab28212f
slowlyRow <18510556554@163.com>
2023-03-11 03:11:31 +0800

    chore: 测试第三方库

---
+++ src/extend/index.js
@@ -0,0 +1,3 @@
+import _ from 'lodash' // 直接引用pakage.json中记录的包名
+const max = _.max([1,5,20])
+console.log('max:', max)
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-  entry: './src/index.js',
+const path = require('path')
+
+module.exports = { 
+  mode: "development",
+  context: path.join(__dirname, './src'),
+  entry: './index.js', // string单入口 []多条件单入口, {}多条件多入口,fn条件返回前三种类型（支持promise）
   output: {
     filename: 'bundle.js',
   },
-  mode: 'development',
 }
