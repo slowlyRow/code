ca6eaa590f05fac4b0426fa6583c3e74d2d567a3
slowlyRow <18510556554@163.com>
2023-03-11 03:14:12 +0800

    test: vue测试各种引用方式和简单实用

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import add from "./add";
+import add from "./add.js";
+// import Vue from 'vue'
+import Vue from 'vue/dist/vue.esm.js'
+
 document.write('i am entry<br/>')
 add()
+
+const test =
+  Vue.component('test', {
+    render: function (createElement) {
+      return createElement('h3', 800088)
+    },
+  })
+const app = new Vue({
+  el: '#a',
+  render(createElement) {
+    return createElement('h1',
+      [
+        'oye', // 内容å
+        createElement('h2', '一则头条'), // h2标签
+        createElement(test), // 组件替换标签字符串
+      ])
+  },
+})
+// .$mount('#a')
+console.log('app : ', app,);
