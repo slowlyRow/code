a9c8107709560129af412a28237cf022c55ea965
slowlyRow <18510556554@163.com>
2023-06-05 02:15:10 +0800

    lint: fix

--- vue2demo/.eslintrc.js
+++ vue2demo/.eslintrc.js
@@ -14,5 +14,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: 'off',
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+    }],
   },
 };
